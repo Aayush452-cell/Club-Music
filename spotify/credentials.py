deba2a084dbdc88974fc4277ec79b9d5fdf28735
Aayush Kumar Singh <malemeaayushkumarsingh@gmail.com>
2022-01-07 16:23:29 +0530

    Update credentials.py

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,4 +1,4 @@
 import os
 CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-REDIRECT_URI = "http://localhost:8000/spotify/redirect/"
+REDIRECT_URI = os.getenv('REDIRECT_URI')
